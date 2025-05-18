a155c742bc582ab41c712ab1e3d6948b95153145
aminetiouk <tiouk.amine@gmail.com>
2025-05-18 11:05:03 +0100

    refactor(header): improve code organization and styling structure

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,46 @@
-import Link from 'next/link';
-import Logo from './Logo';
+import Link from "next/link";
+import Logo from "./logo";
 
 const routes = [
   {
-    name: 'Home',
-    path: '/'
+    name: "Home",
+    path: "/",
   },
   {
-    name: 'All Events',
-    path: '/events/all'
-  }
+    name: "All Events",
+    path: "/events/all",
+  },
 ];
 
 export default function Header() {
   return (
     <header
-      className="flex justify-between items-center h-14 px-3 sm:px-9 border-b
-        border-white/10 "
+      className="
+        flex
+        h-14
+        px-3
+        border-b border-white/10
+        items-center justify-between
+        sm:px-9
+      "
     >
       <Logo />
       <nav>
-        <ul className="flex gap-x-6 text-xl">
-          {routes.map(route => (
-            <li key={route.path} className='text-white/50 hover:text-white transition'>
+        <ul
+          className="
+            flex
+            text-xl
+            gap-x-6
+          "
+        >
+          {routes.map((route) => (
+            <li
+              key={route.path}
+              className="
+                text-white/50
+                transition hover:text-white
+              "
+            >
               <Link href={route.path}>{route.name}</Link>
             </li>
           ))}
