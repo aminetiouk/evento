bd0d7bad08b062a8a6c6633727a532dd072c97d2
aminetiouk <tiouk.amine@gmail.com>
2025-05-18 11:05:03 +0100

    feat(footer): add complete footer with links and styling

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,27 @@
+import Link from 'next/link';
+
+const routes = [
+  {
+    name: 'Terms & Conditions',
+    path: '/terms-conditions'
+  },
+  {
+    name: 'Privacy Policy',
+    path: '/privacy-policy'
+  }
+];
+
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="mt-auto flex justify-between items-center px-3 sm:px-9 text-base text-white/50 h-14">
+      <small>&copy; 2025 Amine. All right reserved</small>
+      <ul className='flex-center space-x-4'>
+        {routes.map(route => (
+          <li key={route.path}>
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
 }
