d9ea094b4e8b7b6d4b3a0f132b4c2869a2933849
aminetiouk <tiouk.amine@gmail.com>
2025-05-18 11:05:03 +0100

    feat(logo): wrap logo image with Link component

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Image src="/logoEvento.png" alt="Evento logo" width={120} height={0}/>
+    <Link href="/">
+      <Image src="/logoEvento.png" alt="Evento logo" width={120} height={0} />
+    </Link>
   );
 }
