058a434e8f7e377a6d5175cb9dc80e3745061b5c
aminetiouk <tiouk.amine@gmail.com>
2025-05-19 20:25:20 +0100

    feat(events): add main layout structure and implement H1 component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,9 @@
+import H1 from '@/components/h1';
+
 export default function events() {
   return (
-    <div>events</div>
-  )
+    <main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
+      <H1>eventsPage</H1>
+    </main>
+  );
 }
