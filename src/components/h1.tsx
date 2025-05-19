0aff15e19dc513fab03b64945ccc165d23b1934c
aminetiouk <tiouk.amine@gmail.com>
2025-05-19 20:24:04 +0100

    feat(components): create reusable H1 heading component

---
+++ src/components/h1.tsx
@@ -0,0 +1,11 @@
+export default function H1({ children }: { children: React.ReactNode }) {
+  return (
+    <h1
+      className="
+          text-3xl font-bold tracking-tight
+          lg:text-6xl"
+    >
+      {children}
+    </h1>
+  );
+}
