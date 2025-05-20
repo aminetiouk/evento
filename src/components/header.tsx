9d2ba51cd92092cba39501398cb1f2f27d19b1ec
aminetiouk <tiouk.amine@gmail.com>
2025-05-20 21:32:55 +0100

    refactor(header): simplify className strings and use cn utility

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,53 +2,38 @@
 
 import Link from 'next/link';
 import Logo from './logo';
-import clsx from 'clsx';
 import { usePathname } from 'next/navigation';
 import { motion } from 'framer-motion';
+import cn from '@/lib/utils';
 
 const routes = [
   {
     name: 'Home',
-    path: '/'
+    path: '/',
   },
   {
     name: 'All Events',
-    path: '/events/all'
-  }
+    path: '/events/all',
+  },
 ];
 
 export default function Header() {
   const activePathname = usePathname();
 
   return (
-    <header
-      className="
-        flex
-        h-14
-        px-3
-        border-b border-white/10
-        items-center justify-between
-        sm:px-9
-      "
-    >
+    <header className="flex h-14 px-3 border-b border-white/10 items-center justify-between sm:px-9">
       <Logo />
+      
       <nav className="h-full">
-        <ul
-          className="
-            flex
-            text-xl
-            gap-x-6
-            h-full
-          "
-        >
-          {routes.map(route => (
+        <ul className="flex text-xl gap-x-6 h-full">
+          {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx(
+              className={cn(
                 'transition relative flex items-center hover:text-white',
                 {
                   'text-white': activePathname === route.path,
-                  'text-white/50': activePathname !== route.path
+                  'text-white/50': activePathname !== route.path,
                 }
               )}
             >
