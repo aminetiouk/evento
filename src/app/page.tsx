ea448adaaba896020bd079b60bca8723377e666e
aminetiouk <tiouk.amine@gmail.com>
2025-05-20 21:32:55 +0100

    refactor(home): clean up and simplify className strings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,51 +4,21 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main
-      className="
-        flex flex-col
-        px-3 pt-36
-        items-center
-      "
-    >
+    <main className="flex flex-col px-3 pt-36 items-center">
       <H1>Find events around you</H1>
-      <p
-        className="
-          mb-12 mt-7
-          text-2xl
-          opacity-75
-          lg:text-3xl
-        "
-      >
+      <p className="mb-12 mt-7 text-2xl opacity-75 lg:text-3xl">
         Browse more than{' '}
-        <span
-          className="
-            font-bold text-accent
-            italic underline
-          "
-        >
-          10,000 events
+        <span className="font-bold text-accent italic underline">
+          10,000 events{' '}
         </span>{' '}
         around you
       </p>
 
       <SearchForm />
 
-      <section
-        className="
-          flex
-          mt-4
-          text-sm text-white/50
-          gap-x-4
-        "
-      >
+      <section className="flex mt-4 text-sm text-white/50 gap-x-4">
         <p>Popular:</p>
-        <div
-          className="
-            space-x-2
-            font-semibold
-          "
-        >
+        <div className="space-x-2 font-semibold">
           <Link href="/events/austin">Austin</Link>
           <Link href="/events/seattle">Seattle</Link>
         </div>
