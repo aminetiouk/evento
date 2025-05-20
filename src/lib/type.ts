8fccb55234050f7f291ed5a6706f8ea028677682
aminetiouk <tiouk.amine@gmail.com>
2025-05-20 19:26:54 +0100

    feat(types): define TEvent type for events data

---
+++ src/lib/type.ts
@@ -0,0 +1,11 @@
+export type TEvent = {
+  city: string;
+  date: Date;
+  description: string;
+  id: number;
+  imageUrl: string;
+  location: string;
+  name: string;
+  organizerName: string;
+  slug: string;
+}
