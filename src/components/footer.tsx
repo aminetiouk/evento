a4e988df0dc0b1e2a6693dc94daee1f35a82906a
aminetiouk <tiouk.amine@gmail.com>
2025-05-21 01:53:57 +0100

    style(footer): clean up and simplify className strings

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -13,24 +13,11 @@ const routes = [
 
 export default function Footer() {
   return (
-    <footer
-      className="
-        flex
-        h-14
-        mt-auto px-3
-        text-xs sm:text-base text-white/25
-        border-t border-white/10
-        justify-between items-center
-        sm:px-9
-      "
+    <footer className="flex h-14 mt-auto px-3 text-xs sm:text-base text-white/25
+      border-t border-white/10 justify-between items-center sm:px-9"
     >
       <small className='text-xs'>&copy; 2025 Amine. All right reserved</small>
-      <ul
-        className="
-          flex-center
-          gap-x-3 sm:gap-x-8
-        "
-      >
+      <ul className="flex-center gap-x-3 sm:gap-x-8">
         {routes.map(route => (
           <li key={route.path}>
             <Link href={route.path}>{route.name}</Link>
