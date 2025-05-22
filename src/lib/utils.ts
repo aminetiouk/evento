179e4d59b8bd1328fe8f9585c3a4fcf037bf21fb
aminetiouk <tiouk.amine@gmail.com>
2025-05-22 23:28:47 +0100

    feat(utils): add sleep helper function for async operations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,9 @@ export default function cn(...inputs: ClassValue[]) {
     twMerge(clsx(inputs))
   )
 }
+
+export async function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
