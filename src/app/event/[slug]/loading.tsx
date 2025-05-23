19f1d6f216ac53feea31d925008c912645b69de7
aminetiouk <tiouk.amine@gmail.com>
2025-05-23 23:43:03 +0100

    feat(loading): add route-specific loading skeletons

---
+++ src/app/event/[slug]/loading.tsx
@@ -0,0 +1,10 @@
+import Skeleton from '@/components/skeleton';
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className='mt-30'>
+      <Skeleton />
+    </div>
+  );
+}
