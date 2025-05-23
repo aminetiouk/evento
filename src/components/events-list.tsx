26382752c2134ba091a47752c829a74ba3828a90
aminetiouk <tiouk.amine@gmail.com>
2025-05-24 00:48:50 +0100

    refactor(events-list): move data fetching into component

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -2,9 +2,16 @@ import { TEvents } from '@/lib/type';
 import EventCard from './event-card';
 
 type EventsListProps = {
-  events: TEvents[]
+  city: string;
 }
-export default function EventsList({ events }: EventsListProps) {
+export default async function EventsList({ city }: EventsListProps) {
+  const response = await fetch(
+      `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
+      {
+        // next: { revalidate: 60 }
+      }
+    );
+    const events: TEvents[] = await response.json();
   return (
     <section className='max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]'>
       {events.map(event => (
