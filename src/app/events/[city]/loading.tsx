e19bdb8d17b3561e99af4a94e2071803e1adabb1
aminetiouk <tiouk.amine@gmail.com>
2025-05-24 00:48:50 +0100

    feat(loading): implement animated skeleton loading cards

--- src/app/events/[city]/loading.tsx
+++ src/app/events/[city]/loading.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 export default function Loading() {
   return (
-    <div>Loading...</div>
-  )
+    <div className='max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]'>
+      {Array.from({length: 6}).map((_ , index) => (
+        <Image key={index} className='animate-pulse' src='/skeleton.svg' alt='skeleton list card' height={100} width={300} />
+      ))}
+    </div>
+  );
 }
