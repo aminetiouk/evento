518175176aea359c04c8651ac696fcb6abaf0a45
aminetiouk <tiouk.amine@gmail.com>
2025-05-26 22:48:08 +0100

    feat(api): add getEventData and getEventsData utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,36 @@
-import clsx, { ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import clsx, { ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export default function cn(...inputs: ClassValue[]) {
-  return (
-    twMerge(clsx(inputs))
-  )
+  return twMerge(clsx(inputs));
 }
 
 export async function sleep(ms: number) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
 }
 
 export function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function getEventsData(city: string) {
+  return fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`,
+    {
+      next: { revalidate: 60 }
+    }
+  );
+}
+export async function getEventData(slug: string) {
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`,
+    {
+      next: { revalidate: 60 }
+    }
+  );
+  const event = await response.json();
+
+  return event;
 }
