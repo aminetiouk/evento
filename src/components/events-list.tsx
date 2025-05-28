8b35865aac9085384c61fc6b853c64e2e1621f26
aminetiouk <tiouk.amine@gmail.com>
2025-05-28 20:45:06 +0100

    refactor(events): simplify data fetching and typing

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,5 +1,3 @@
-
-import { EventoEvent } from '@/generated/prisma';
 import EventCard from './event-card';
 import { getEventsData } from '@/lib/utils';
 
@@ -7,9 +5,8 @@ type EventsListProps = {
   city: string;
 };
 export default async function EventsList({ city }: EventsListProps) {
-  const response = await getEventsData(city);
+  const events = await getEventsData(city);
 
-  const events: EventoEvent[] = await response.json();
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map(event => (
