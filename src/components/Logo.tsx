81a5f0583f6fc4dd7e114736bcd938b719f22b2e
aminetiouk <tiouk.amine@gmail.com>
2025-05-28 20:45:06 +0100

    refactor(logo): remove Link wrapper for better flexibility

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,22 +1,19 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Logo() {
   return (
-    <Link href="/">
-      <Image
-        src="/logoEvento.png"
-        alt="Evento logo"
-        width={120}
-        height={50}
-        quality={100}
-        priority
-        style={{
-          objectFit: 'contain',
-          width: 'auto',
-          height: 'auto'
-        }}
-      />
-    </Link>
+    <Image
+      src="/logoEvento.png"
+      alt="Evento logo"
+      width={120}
+      height={50}
+      quality={100}
+      priority
+      style={{
+        objectFit: 'contain',
+        width: 'auto',
+        height: 'auto'
+      }}
+    />
   );
 }
