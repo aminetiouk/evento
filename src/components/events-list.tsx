182cc78a41fd25d700d089decbd5f7433b809faf
aminetiouk <tiouk.amine@gmail.com>
2025-05-30 12:55:06 +0100

    refactor(events-list): redirect to last page if requested page exceeds total

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,14 +1,17 @@
 import EventCard from './event-card';
 import { getEventsData } from '@/lib/utils';
 import PaginationControls from './pagination-controls';
+import { redirect } from 'next/navigation';
 
 type EventsListProps = {
   city: string;
-  page: number;
+  page?: number;
 };
 
-export default async function EventsList({ city, page }: EventsListProps) {
+export default async function EventsList({ city, page = 1 }: EventsListProps) {
   const { totalPages, events } = await getEventsData(city, page);
+
+  if (page > totalPages) redirect(`/events/${city}?page=${totalPages}`);
 
   const prevPage = page > 1 ? `/events/${city}?page=${page - 1}` : '';
   const nextPage = page < totalPages ? `/events/${city}?page=${page + 1}` : '';
