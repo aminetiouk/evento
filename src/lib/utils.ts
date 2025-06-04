4146840c652f5d3ac7fc681ff7300a879332fab2
aminetiouk <tiouk.amine@gmail.com>
2025-06-04 23:44:42 +0100

    refactor(utils): remove Prisma and data fetching logic, leaving only shared utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,66 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-import clsx, { ClassValue } from 'clsx';
-import { notFound } from 'next/navigation';
-import { twMerge } from 'tailwind-merge';
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-const prisma = new PrismaClient();
-
-export default function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export async function sleep(ms: number) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms);
-  });
 }
 
 export function capitalize(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export async function getEventsData(city: string, page = 1) {
-  if (city !== 'all') {
-    const cityExists = await prisma.eventoEvent.findFirst({
-      where: {
-        city: capitalize(city)
-      }
-    });
-
-    if (!cityExists) return notFound();
-  }
-
-  const whereClause = {
-    city: city === 'all' ? undefined : capitalize(city)
-  };
-
-  const events = await prisma.eventoEvent.findMany({
-    where: whereClause,
-    orderBy: {
-      date: 'asc'
-    },
-    take: 6,
-    skip: (page - 1) * 6
+export async function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
   });
-
-  const totalCount = await prisma.eventoEvent.count({
-    where: whereClause
-  });
-
-  return {
-    totalPages: Math.ceil(totalCount / 6),
-    events
-  };
-}
-
-export async function getEventData(slug: string) {
-  if (!slug) return notFound();
-
-  const event = await prisma.eventoEvent.findUnique({
-    where: { slug }
-  });
-
-  if (!event) return notFound();
-
-  return event;
 }
