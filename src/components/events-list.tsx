34c8b9bce54c7335a66924113f3592d8f12fe7dd
aminetiouk <tiouk.amine@gmail.com>
2025-06-05 20:00:06 +0100

    refactor(events-list): update import for getEventsData and adjust pagination logic

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,5 +1,5 @@
 import EventCard from './event-card';
-import { getEventsData } from '@/lib/utils';
+import { getEventsData } from '@/lib/server-utils';
 import PaginationControls from './pagination-controls';
 import { redirect } from 'next/navigation';
 
@@ -9,12 +9,12 @@ type EventsListProps = {
 };
 
 export default async function EventsList({ city, page = 1 }: EventsListProps) {
-  const { totalPages, events } = await getEventsData(city, page);
+  const { totalCount, events } = await getEventsData(city, page);
 
-  if (page > totalPages) redirect(`/events/${city}?page=${totalPages}`);
+  if (page > totalCount) redirect(`/events/${city}?page=${totalCount}`);
 
   const prevPage = page > 1 ? `/events/${city}?page=${page - 1}` : '';
-  const nextPage = page < totalPages ? `/events/${city}?page=${page + 1}` : '';
+  const nextPage = page < totalCount ? `/events/${city}?page=${page + 1}` : '';
 
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
