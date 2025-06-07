e627269cc69d8b188c7426eebda4bf4b21ca49fb
aminetiouk <tiouk.amine@gmail.com>
2025-06-07 01:09:35 +0100

    refactor(events-page): update Props type for awaited params/searchParams, simplify page parsing and remove Zod validation

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -3,38 +3,26 @@ import H1 from '@/components/h1';
 import { Suspense } from 'react';
 import Loading from './loading';
 import { capitalize } from '@/lib/utils';
-import { z } from 'zod';
-import { notFound } from 'next/navigation';
 
-type TProps = {
-  params: {
+type Props = {
+  params: Promise<{
     city: string;
-  };
+  }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-type TEventsPageProps = TProps & {
-  searchParams: { [key: string]: string | string[] | undefined };
-};
-
-export function generateMetadata({ params }: TProps) {
-  const city = params.city;
+export async function generateMetadata({ params }: Props) {
+  const { city } = await params;
 
   return {
     title: city === 'all' ? 'All Events' : `Events in ${capitalize(city)}`
   };
 }
 
-const pageNumberSchema = z.coerce.number().int().positive().optional();
 
-export default async function EventsPage({
-  params,
-  searchParams
-}: TEventsPageProps) {
-  const { city } = params;
-  const parsePage = pageNumberSchema.safeParse(searchParams.page);
-  if (!parsePage.success) {
-    return notFound();
-  }
+export default async function EventsPage({ params, searchParams }: Props) {
+  const { city } = await params;
+  const { page = '1' } = await searchParams;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px]">
@@ -42,8 +30,8 @@ export default async function EventsPage({
         {city === 'all' && 'All Events'}
         {city !== 'all' && `Events in ${capitalize(city)}`}
       </H1>
-      <Suspense key={city + parsePage} fallback={<Loading />}>
-        <EventsList city={city} page={parsePage.data} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} page={+page} />
       </Suspense>
     </main>
   );
